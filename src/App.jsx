45651650ea4c541b59ddd059f880384ab676aba9
Mahbuba Islam Priya <mahbubacse18@gmail.com>
2024-05-26 19:31:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+      <h1 className="text-3xl bg-slate-400 p-4 font-bold underline">
+      Hello world!
+    </h1>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
